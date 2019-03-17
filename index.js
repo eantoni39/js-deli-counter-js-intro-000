94679ea6755e23427ae8d63fcd5ac5ec832a319e
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-17 22:50:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function nowServing(line) {
 }
 
 }
+
+function currentLine (line) {
+  if (line.length === 0) {
+      return 'The line is currently empty'
+    } else { for (var i=0;i<line.length;i++) {
+      return `The line is currently: ${i+1}.${line[i]}`
+    }}
+}
